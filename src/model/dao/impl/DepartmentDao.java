306935d0a86ed8045aa04e797f7d7e9f3b1c407c
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-12 22:46:43 -0300

    feat: default methods in DepartmentDAO

--- src/model/dao/impl/DepartmentDao.java
+++ src/model/dao/impl/DepartmentDao.java
@@ -3,5 +3,31 @@ package model.dao.impl;
 import model.dao.ObjectDao;
 import model.entities.Department;
 
+import java.util.List;
+
 public class DepartmentDao implements ObjectDao<Department> {
+    @Override
+    public void insert(Department entity) {
+
+    }
+
+    @Override
+    public void update(Department entity) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department getById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> getAll() {
+        return null;
+    }
 }
